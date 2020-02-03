65a20b5a911ba653501d5db2521e6105f59815b4
mmk1026 <m35xoxo@gmail.com>
2020-02-03 20:49:43 +0900

    frist commit

---
+++ src/main/java/com/example/demo/trySpring/HelloService.java
@@ -0,0 +1,30 @@
+package com.example.demo.trySpring;
+
+import java.util.Map;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+	private HelloRepository helloRepository;
+	
+	public Employee findOne(int id) {
+		
+		// 1件検索実行
+		Map<String, Object> map = helloRepository.findOne(id);
+		
+		//Mapから値を取得
+		int employeeId = (Integer)map.get("employee_id");
+		String employeeName = (String)map.get("employee_name");
+		int age = (Integer)map.get("age");
+		
+		//Employeeクラスに値をセット
+		Employee employee = new Employee();
+		employee.setEmployeeId(employeeId);
+		employee.setEmoployeeName(employeeName);
+		employee.setAge(age);
+		
+		return employee;
+	}
+}
+
+
