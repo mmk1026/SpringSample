209d63a34afa1e24e35aed4be0cead3af1fc6c98
mmk1026 <m35xoxo@gmail.com>
2020-02-15 10:20:09 +0900

    a

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,10 @@
 INSERT INTO employee(employee_id, employee_name, age)
 VALUES(1,'山田太郎',30);
+
+/* ユーザーマスタのデータ（アドミン権限）*/
+INSERT INTO m_user (user_id,password,user_name,birthday,age,marriage,role)
+VALUES('mamiko@xxx.co.jp','$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa','かわせまみこ','1993-10-26',26,false,'ROLE_ADMIN');
+
+/* ユーザーマスタのデータ（一般権限）*/
+INSERT INTO m_user (User_id,password,user_name,birthday,age,marriage,role)
+VALUES('yamada@xxx.co.jp','$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa','山田太郎','1986-11-05',31,false,'ROLE_GENERAL');
